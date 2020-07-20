5d54b9a45019183d7b4e5278eb1d7170cddf6ff4
Alvaro Reyes <areyes.088@gmail.com>
2020-07-19 20:11:59 -0400

    docs agregad

---
+++ docs/precache-manifest.0a146e634f287c068605492dbaed691e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d07b795c7fc1d70e0d9710dbefb031",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d55db160c41bb8cf9f",
+    "url": "/static/css/main.4ece02c6.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d55db160c41bb8cf9f",
+    "url": "/static/js/main.47ce58f4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
